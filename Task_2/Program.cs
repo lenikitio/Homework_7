95fbf75cb02eed5ac4278104df98f24cebf2a9a1
nikita <tolstoguzov.nikita98@mail.ru>
2023-03-08 16:56:23 +0300

    Задача 2 со звёздочкой

---
+++ Task_2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Дополнительная задача 2 (задача со звёздочкой): 
+// Вывести первые n строк треугольника Паскаля. Реализовать вывод в виде равнобедренного треугольника.
+
+void GetPascalTriangle(int N)
+{
+    int Strok = N;
+    int Stolb = 2*N - 1;
+    int Lcount = ((Stolb + 1) / 2) - 1;
+    int Rcount = Lcount;
+    int[,] PascalTriangle = new int[Strok,Stolb];
+    for(int i = 0; i < Strok; i++)
+    {
+        for(int j = 0; j < Stolb; j++)
+        {
+            if(j == Lcount || j == Rcount)
+            {
+            PascalTriangle[i,j] = 1;
+            Console.Write(PascalTriangle[i,j]);
+            }
+            else if(j > Lcount && j < Rcount)
+            {
+                PascalTriangle[i,j] = PascalTriangle[i - 1, j - 1] + PascalTriangle[i - 1, j + 1];
+                if(PascalTriangle[i,j] == 0) Console.Write(" ");
+                else Console.Write(PascalTriangle[i,j]);
+            }
+            else Console.Write(" ");
+        }
+        Lcount--;
+        Rcount++;
+        Console.WriteLine("");
+    }
+}
+
+GetPascalTriangle(7);
